aa8c93f33643556027dd3d5fc719c5dd5c028ee0
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-09-24 09:00:36 +0200

    Add files via upload

---
+++ tests/test_interface.py
@@ -0,0 +1,21 @@
+
+import random
+from unittest.main import main
+import requests
+import time
+
+import unittest
+
+class TestInteface(unittest.TestCase):
+    
+    def test_interface(self):
+
+        secret = random.randint(0, 10000)    
+        requests.post(f'http://myserver:9999/{secret}')
+        time.sleep(1)
+        r = requests.get(f'http://myclient:9998/')
+        self.assertEqual(secret, int(r.content))
+
+if __name__ == "__main__":
+    test = TestInteface()
+    test.test_interface()
